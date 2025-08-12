977ad7976838c1ef38708349ec7b8306b14b900b
jocastanheira <jocastanheira@deloitte.pt>
2025-08-12 11:17:56 +0100

    feat: refactor Board component and add CardBoard for issue display

--- src/pages/Board/BoardPage.tsx
+++ src/pages/Board/BoardPage.tsx
@@ -3,7 +3,7 @@ import Board from "../../components/board/Board";
 export default function BoardPage() {
   return (
     <main id="#main" className="min-h-[100vh]">
-      <div className="aims-dashboard-container">
+      <div className="">
         <Board />
       </div>
     </main>
